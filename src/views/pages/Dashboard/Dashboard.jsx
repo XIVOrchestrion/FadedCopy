99d801667a8252ba1fdddc27c602d69e30df0058
Liam Morgan <lite@getignited.co.uk>
2019-01-10 01:05:31 +0000

    Setting up basic views

---
+++ src/views/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Dashboard extends React.Component {
+
+  render() {
+
+    return (
+      <main>
+        <h1>Hello DASHBOARD world</h1>
+      </main>
+    )
+  }
+}
+
+export default Dashboard
---
+++ src/views/pages/Settings/Settings.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Settings extends React.Component {
+
+  render() {
+
+    return(
+      <main>
+        <h1>Hello SETTINGS world</h1>
+      </main>
+    )
+  }
+}
+
+export default Settings
