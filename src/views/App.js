359217574abd753ac8928f223f6fb2a58a951853
Liam Morgan <lite@getignited.co.uk>
2019-01-10 01:05:50 +0000

    Routing and demoing for login process

--- src/views/App.js
+++ src/views/App.js
@@ -1,26 +1,100 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { Switch, Route, Redirect, Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { checkUserData, userLogout } from '../store/app'
+
+import {
+  Login,
+  Settings,
+  Welcome,
+} from './pages'
+
+import './App.css'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.checkUserData()
+  }
+
+  loginRoute = ({ component: Component, ...rest }) => {
+    const auth = this.props.user
+    return (
+      <Route
+        {...rest}
+        render={props =>
+          auth
+            ? <Redirect to={{ pathname: '/settings', state: { from: props.location } }} />
+            : <Component {...props} />
+        }
+      />
+    )
+  }
+
+  privateRoute = ({ component: Component, ...rest }) => {
+    const auth = this.props.user
+    return (
+      <Route
+        {...rest}
+        render={props =>
+          auth
+            ? <Component {...props} />
+            : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+        }
+      />
+    )
+  }
+
   render() {
+    const {
+      user
+    } = this.props
+
+    const LoginRoute = this.loginRoute
+    const PrivateRoute = this.privateRoute
+
+    if (this.props.user === false) {
+      return (
+        <div className="App">
+
+        </div>
+      )
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Link to={{ pathname: '/login', state: { from: this.props.location } }}>Log in</Link>
+        {user &&
+          <button onClick={this.props.userLogout}>
+            Log out
+          </button>
+        }
+
+        <Switch>
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Welcome} />
+          <Route path={`${process.env.PUBLIC_URL}/login`} component={Login} />
+          <LoginRoute path={`${process.env.PUBLIC_URL}/register`} component={Login} />
+          <PrivateRoute path={`${process.env.PUBLIC_URL}/settings`} component={Settings} />
+        </Switch>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  const { user } = state.app
+
+  return {
+    user,
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  checkUserData: bindActionCreators(checkUserData, dispatch),
+  userLogout: bindActionCreators(userLogout, dispatch),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+// 6715878
