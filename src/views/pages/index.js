53f5688c616e15372492bcbc6040eb8ba828a4a7
Liam Morgan <lite@getignited.co.uk>
2019-01-11 10:30:09 +0000

    Replaced Welcome with Home, added Register

--- src/views/pages/index.js
+++ src/views/pages/index.js
@@ -1,5 +1,5 @@
-export { default as Dashboard } from './Dashboard'
+export { default as Home } from './Home'
 export { default as Login } from './Login'
 // export { default as Profile } from './Profile'
+export { default as Register } from './Register'
 export { default as Settings } from './Settings'
-export { default as Welcome } from './Welcome'
