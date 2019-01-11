ff1de42c6c831b339fb565f4937edcc714dc380a
Liam Morgan <lite@getignited.co.uk>
2019-01-11 10:28:11 +0000

    Moved Dashboard and Welcome views to Home

---
+++ src/views/pages/Home/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import HomeDashboard from './HomeDashboard'
+import HomeWelcome from './HomeWelcome'
+
+class Home extends React.Component {
+
+  render() {
+
+    return(
+      <React.Fragment>
+        <div>
+          {this.props.isAuthenticated ? (
+            <HomeDashboard />
+          ) : (
+            <HomeWelcome />
+          )}
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.app.isAuthenticated,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
---
+++ src/views/pages/Home/HomeDashboard.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { fetchSongsIfNeeded } from '../../../store/dashboard'
+
+import { SongCard } from '../../../components'
+
+class Dashboard extends React.Component {
+  componentDidMount() {
+    this.props.fetchSongsIfNeeded()
+  }
+
+  render() {
+
+    console.log(this.props.songs)
+
+    return (
+      <main>
+        <h1>Hello DASHBOARD world</h1>
+        {this.props.songs.map(item => (
+          <SongCard
+            key={item.id}
+            {...item}
+          />
+        ))}
+      </main>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  const {
+    error,
+    isFetching,
+    lastUpdated,
+    songs,
+  } = state.dashboard
+
+  return {
+    error,
+    isFetching,
+    lastUpdated,
+    songs,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchSongsIfNeeded: bindActionCreators(fetchSongsIfNeeded, dispatch),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
