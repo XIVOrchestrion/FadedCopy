e6ca9a16c54c0bc92db7d960c35d132644cbf82d
Liam Morgan <lite@getignited.co.uk>
2019-01-14 14:34:38 +0000

    Alert component setup

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as Alert } from './Alert'
 export { default as Button } from './Button'
 export { default as Character } from './Character'
 export { default as Header } from './Header'
