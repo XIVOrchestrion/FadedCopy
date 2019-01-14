5a08076da5dc66447094867d8abe17fabde89fcc
Liam Morgan <lite@getignited.co.uk>
2019-01-14 00:10:46 +0000

    Basic layout on Character component

---
+++ src/components/Character/Character.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import styles from './Character.module.scss'
+
+const Character = ({
+  avatar,
+  id,
+  name,
+  server,
+}) => {
+  return (
+    <div className={styles.root}>
+      <img
+        src={avatar}
+        className={styles.image}
+        alt=""
+      />
+      <div className={styles.content}>
+        <span className={styles.name}>
+          { name }
+        </span>
+        <span className={styles.server}>
+          { server }
+        </span>
+      </div>
+    </div>
+  )
+}
+
+Character.propTypes = {
+  avatar: PropTypes.string,
+  id: PropTypes.number,
+  name: PropTypes.string,
+  server: PropTypes.string,
+}
+
+export default Character
