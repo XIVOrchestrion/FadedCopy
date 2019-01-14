e8c6e9ba94650f8cf023699400dcc9fd893b7d3b
Liam Morgan <lite@getignited.co.uk>
2019-01-14 15:21:43 +0000

    Improving SongCard styling

--- src/components/SongCard/SongCard.jsx
+++ src/components/SongCard/SongCard.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import styles from './SongCard.module.scss'
+
 const SongCard = ({
   desc,
   id,
   name,
+  tracking,
   uiCat,
   uiId,
   uiOrder,
 }) => {
 
   return(
-    <div>
-      { uiId }. { name.en }
+    <div className={styles.root}>
+      {tracking &&
+        <input type="checkbox" />
+      }
+      <div className={styles.number}>
+        { uiId }.
+      </div>
+      <div className={styles.name}>
+        { name.en }
+      </div>
+      <div className={styles.desc}>
+        { desc.en }
+      </div>
     </div>
   )
 }
@@ -21,9 +35,17 @@ SongCard.propTypes = {
   desc: PropTypes.object,
   id: PropTypes.number,
   name: PropTypes.object,
+  /**
+   * If `true`, enables tracking and logging functionality
+   */
+  tracking: PropTypes.bool.isRequired,
   uiCat: PropTypes.number,
   uiId: PropTypes.number,
   uiOrder: PropTypes.number,
+}
+
+SongCard.defaultProps = {
+  tracking: false,
 }
 
 export default SongCard
