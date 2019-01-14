6a84710688a4b7d93957c0bd37f0ae60f21be35a
Liam Morgan <lite@getignited.co.uk>
2019-01-14 00:12:43 +0000

    Added Server data

---
+++ src/data/servers.js
@@ -0,0 +1,374 @@
+export default [
+  {
+    name: "Aether",
+    id: 0,
+    region: "NA",
+    servers: [
+      {
+        id: 0,
+        key: "adamantoise",
+        name: "Adamantoise",
+      },
+      {
+        id: 1,
+        key: "balmung",
+        name: "Balmung",
+      },
+      {
+        id: 2,
+        key: "cactuar",
+        name: "Cactuar",
+      },
+      {
+        id: 3,
+        key: "coeurl",
+        name: "Coeurl",
+      },
+      {
+        id: 4,
+        key: "faerie",
+        name: "Faerie",
+      },
+      {
+        id: 5,
+        key: "gilgamesh",
+        name: "Gilgamesh",
+      },
+      {
+        id: 6,
+        key: "goblin",
+        name: "Goblin",
+      },
+      {
+        id: 7,
+        key: "jenova",
+        name: "Jenova",
+      },
+      {
+        id: 8,
+        key: "mateus",
+        name: "Mateus",
+      },
+      {
+        id: 9,
+        key: "midgardsormr",
+        name: "Midgardsormr",
+      },
+      {
+        id: 10,
+        key: "sargantas",
+        name: "Sargantas",
+      },
+      {
+        id: 11,
+        key: "siren",
+        name: "Siren",
+      },
+      {
+        id: 12,
+        key: "zalera",
+        name: "Zalera",
+      },
+    ],
+  },
+  {
+    name: "Chaos",
+    id: 1,
+    region: "EU",
+    servers: [
+      {
+        id: 0,
+        key: "cerberus",
+        name: "Cerberus",
+      },
+      {
+        id: 1,
+        key: "lich",
+        name: "Lich",
+      },
+      {
+        id: 2,
+        key: "louisoux",
+        name: "Louisoix",
+      },
+      {
+        id: 3,
+        key: "moogle",
+        name: "Moogle",
+      },
+      {
+        id: 4,
+        key: "odin",
+        name: "Odin",
+      },
+      {
+        id: 5,
+        key: "omega",
+        name: "Omega",
+      },
+      {
+        id: 6,
+        key: "phoenix",
+        name: "Phoenix",
+      },
+      {
+        id: 7,
+        key: "ragnarok",
+        name: "Ragnarok",
+      },
+      {
+        id: 8,
+        key: "shiva",
+        name: "Shiva",
+      },
+      {
+        id: 9,
+        key: "zodiak",
+        name: "Zodiark",
+      },
+    ]
+  },
+  {
+    name: "Elemental",
+    id: 2,
+    region: "JP",
+    servers: [
+      {
+        id: 0,
+        key: "aegis",
+        name: "Aegis",
+      },
+      {
+        id: 1,
+        key: "atomos",
+        name: "Atomos",
+      },
+      {
+        id: 2,
+        key: "carbuncle",
+        name: "Carbuncle",
+      },
+      {
+        id: 3,
+        key: "garuda",
+        name: "Garuda",
+      },
+      {
+        id: 4,
+        key: "gungir",
+        name: "Gungnir",
+      },
+      {
+        id: 5,
+        key: "kujata",
+        name: "Kujata",
+      },
+      {
+        id: 6,
+        key: "ramuh",
+        name: "Ramuh",
+      },
+      {
+        id: 7,
+        key: "tonberry",
+        name: "Tonberry",
+      },
+      {
+        id: 8,
+        key: "typhon",
+        name: "Typhon",
+      },
+      {
+        id: 9,
+        key: "unicorn",
+        name: "Unicorn",
+      },
+    ]
+  },
+  {
+    name: "Gaia",
+    id: 3,
+    region: "JP",
+    servers: [
+      {
+        id: 0,
+        key: "alexander",
+        name: "Alexander",
+      },
+      {
+        id: 1,
+        key: "bahamut",
+        name: "Bahamut",
+      },
+      {
+        id: 2,
+        key: "durandal",
+        name: "Durandal",
+      },
+      {
+        id: 3,
+        key: "fenrir",
+        name: "Fenrir",
+      },
+      {
+        id: 4,
+        key: "ifrit",
+        name: "Ifrit",
+      },
+      {
+        id: 5,
+        key: "ridill",
+        name: "Ridill",
+      },
+      {
+        id: 6,
+        key: "tiamat",
+        name: "Tiamat",
+      },
+      {
+        id: 7,
+        key: "ultima",
+        name: "Ultima",
+      },
+      {
+        id: 8,
+        key: "valefor",
+        name: "Valefor",
+      },
+      {
+        id: 9,
+        key: "yojimbo",
+        name: "Yojimbo",
+      },
+      {
+        id: 10,
+        key: "zeromus",
+        name: "Zeromus",
+      },
+    ]
+  },
+  {
+    name: "Mana",
+    id: 4,
+    region: "JP",
+    servers: [
+      {
+        id: 0,
+        key: "anima",
+        name: "Anima",
+      },
+      {
+        id: 1,
+        key: "asura",
+        name: "Asura",
+      },
+      {
+        id: 2,
+        key: "belias",
+        name: "Belias",
+      },
+      {
+        id: 3,
+        key: "chocobo",
+        name: "Chocobo",
+      },
+      {
+        id: 4,
+        key: "hades",
+        name: "Hades",
+      },
+      {
+        id: 5,
+        key: "ixion",
+        name: "Ixion",
+      },
+      {
+        id: 6,
+        key: "mandragora",
+        name: "Mandragora",
+      },
+      {
+        id: 7,
+        key: "masamune",
+        name: "Masamune",
+      },
+      {
+        id: 8,
+        key: "pandaemonium",
+        name: "Pandaemonium",
+      },
+      {
+        id: 9,
+        key: "shinryu",
+        name: "Shinryu",
+      },
+      {
+        id: 10,
+        key: "titan",
+        name: "Titan",
+      },
+    ]
+  },
+  {
+    name: "Primal",
+    id: 5,
+    region: "NA",
+    servers: [
+      {
+        id: 0,
+        key: "behemoth",
+        name: "Behemoth",
+      },
+      {
+        id: 1,
+        key: "brynhildr",
+        name: "Brynhildr",
+      },
+      {
+        id: 2,
+        key: "diabolos",
+        name: "Diabolos",
+      },
+      {
+        id: 3,
+        key: "excalibur",
+        name: "Excalibur",
+      },
+      {
+        id: 4,
+        key: "exodus",
+        name: "Exodus",
+      },
+      {
+        id: 5,
+        key: "famfrit",
+        name: "Famfrit",
+      },
+      {
+        id: 6,
+        key: "hyperion",
+        name: "Hyperion",
+      },
+      {
+        id: 7,
+        key: "lamia",
+        name: "Lamia",
+      },
+      {
+        id: 8,
+        key: "leviathan",
+        name: "Leviathan",
+      },
+      {
+        id: 9,
+        key: "malboro",
+        name: "Malboro",
+      },
+      {
+        id: 10,
+        key: "ultros",
+        name: "Ultros",
+      },
+    ]
+  },
+]
