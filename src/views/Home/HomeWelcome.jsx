312b626caf484059a329e430327e85480c246c83
Liam Morgan <lite@getignited.co.uk>
2019-01-14 00:14:20 +0000

    Functionality to alter homepage state

---
+++ src/views/Home/HomeWelcome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Welcome extends React.Component {
+
+  render() {
+
+    return(
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    )
+  }
+}
+
+export default Welcome
