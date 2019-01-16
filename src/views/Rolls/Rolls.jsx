462248d7d75113e6231139d8de12d8236235a518
Liam Morgan <lite@getignited.co.uk>
2019-01-16 08:58:03 +0000

    Logic and loading tweaks

--- src/views/Rolls/Rolls.jsx
+++ src/views/Rolls/Rolls.jsx
@@ -2,18 +2,30 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
-import { fetchSongsIfNeeded } from '../../store/dashboard'
+import { getProgress, updateTrack } from '../../store/dashboard'
 
-import { SongCard } from '../../components'
+import { SongCard, Alert } from '../../components'
 
 class Rolls extends React.Component {
   componentDidMount() {
-    this.props.fetchSongsIfNeeded()
+    this.props.getProgress()
+  }
+
+  handleChange = (e) => {
+    console.log(e.target, e.target.checked)
+    this.props.updateTrack(e.target.id, e.target.checked)
   }
 
   render() {
+    const {
+      activeCharacter,
+      authenticated,
+      status,
+      obtained,
+      songs,
+    } = this.props
 
-    if (!this.props.songs)
+    if (!status)
       return (
         <main>
           <span>Loading songs</span>
@@ -22,13 +34,37 @@ class Rolls extends React.Component {
 
     return (
       <main>
-        {this.props.songs.map(item => (
-          <SongCard
-            key={item.id}
-            {...item}
-            tracking
-          />
-        ))}
+        {!authenticated &&
+          <Alert type="message">
+            Start tracking your progress by signing up for an account or logging in!
+          </Alert>
+        }
+
+        {(authenticated && !activeCharacter) &&
+          <Alert>
+            You don't have a character assigned to your account yet.
+          </Alert>
+        }
+
+        {songs.map(item => {
+          let extraProps = {}
+          if (activeCharacter) {
+            const isChecked = obtained[activeCharacter] ? obtained[activeCharacter].includes(item.id.toString()) : false
+            extraProps = {
+              checked: isChecked,
+              handleChange: this.handleChange,
+              tracking: true,
+            }
+          }
+
+          return (
+            <SongCard
+              key={item.id}
+              {...item}
+              {...extraProps}
+            />
+          )
+        })}
       </main>
     )
   }
@@ -36,22 +72,34 @@ class Rolls extends React.Component {
 
 const mapStateToProps = (state) => {
   const {
+    activeCharacter,
+    authenticated,
     error,
+  } = state.app
+
+  const {
     isFetching,
     lastUpdated,
     songs,
+    status,
+    obtained
   } = state.dashboard
 
   return {
+    activeCharacter,
+    authenticated,
     error,
     isFetching,
     lastUpdated,
     songs,
+    status,
+    obtained,
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchSongsIfNeeded: bindActionCreators(fetchSongsIfNeeded, dispatch),
+  getProgress: bindActionCreators(getProgress, dispatch),
+  updateTrack: bindActionCreators(updateTrack, dispatch),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Rolls)
