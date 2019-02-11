9794193117e98ac909bbb2c614884553b76bbf94
Liam Morgan <lite@getignited.co.uk>
2019-02-11 08:25:58 +0000

    Introductory sizing options for Icon

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,33 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import * as icons from './Icon.assets'
 
 import styles from './Icon.module.scss'
 
 const Icon = ({
+  classes,
   hidden,
   icon,
   paths,
+  size,
   viewBox,
 }) => {
   const iconPaths = icon ? icons[icon] : paths
 
   return (
-    <svg
-      className={styles.root}
-      viewBox={`0 0 ${viewBox[0]} ${viewBox[1]}`}
-      aria-hidden={hidden}
-    >
-      {iconPaths && iconPaths.map((pathProps, i) => (
-        <path {...pathProps} key={i} />
-      ))}
-    </svg>
+    <span className={
+      classNames(styles.root, {
+        [styles.small]: size === 'small',
+      },
+      classes)
+    }>
+      <svg
+        className={styles.svg}
+        viewBox={`0 0 ${viewBox[0]} ${viewBox[1]}`}
+        aria-hidden={hidden}
+      >
+        {iconPaths && iconPaths.map((pathProps, i) => (
+          <path {...pathProps} key={i} />
+        ))}
+      </svg>
+    </span>
   )
 }
 
 Icon.propTypes = {
+  classes: PropTypes.string,
   hidden: PropTypes.bool,
   icon: PropTypes.string,
+  size: PropTypes.string,
   viewBox: PropTypes.array,
 }
 
